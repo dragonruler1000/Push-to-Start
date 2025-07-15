ad86e9a9d8bbe4556170c17e87a281e704e60985
dragonruler1000 <github@me.minecraftchest2.us>
2025-07-15 08:36:29 -0500

    got camera switching working.

--- push-to-start/scenes/game_scene/levels/level_1.gd
+++ push-to-start/scenes/game_scene/levels/level_1.gd
@@ -3,11 +3,12 @@ extends Node2D
 func _ready():
 	# This function is called when the node is added to the scene.
 	print("Level 1 is ready!")
-	var area2 = get_node("cameraview2")
-	area2.connect("area_entered", self, "_on_cameraview_2_area_entered")
 
-func _on_cameraview_2_area_entered(_area:Area2D) -> void:
-	get_viewport().get_camera().current = false
-	$"Camera2".current = true
-	print("Camera switched to Camera2")
-    print("hi")
+func _on_cameraview_2_body_entered(_body: Node2D) -> void:
+	print("area 2  entered")
+	$"Camera2".make_current()
+
+
+func _on_cameraview_1_body_entered(body: Node2D) -> void:
+	print("area 1 entered")
+	$"Camera1".make_current()
