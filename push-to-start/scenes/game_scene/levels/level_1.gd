cdefa91d42b7d05ab04d5e746c826f1f9df2fa6c
dragonruler1000 <github@me.minecraftchest2.us>
2025-07-14 22:19:12 -0500

    Adds camera switching and movement tutorial UI
    Introduces functionality to switch cameras when the player enters specific areas. Adds a movement tutorial UI with instructions for player controls. Updates scene layout and collision shapes for improved interaction and clarity.

--- push-to-start/scenes/game_scene/levels/level_1.gd
+++ push-to-start/scenes/game_scene/levels/level_1.gd
@@ -1,8 +1,12 @@
 extends Node2D
 
+func _ready():
+	# This function is called when the node is added to the scene.
+	print("Level 1 is ready!")
+	var area2 = get_node("cameraview2")
+	area2.connect("area_entered", self, "_on_cameraview_2_area_entered")
 
-func playerspawn():
-    print("Player spawn called")
-    print(self)
-    get_viewport().get_camera().current = false
-    $"Camera1".current = true
+func _on_cameraview_2_area_entered(_area:Area2D) -> void:
+	get_viewport().get_camera().current = false
+	$"Camera2".current = true
+	print("Camera switched to Camera2")
