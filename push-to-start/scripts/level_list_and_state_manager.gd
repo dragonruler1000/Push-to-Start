59df32a40e18c8bbbb9c82475b50b5cfffefbfb1
dragonruler1000 <github@me.minecraftchest2.us>
2025-07-18 08:53:16 -0500

    Added a tile set. trying to figure out why the levels don't load.

--- push-to-start/scripts/level_list_and_state_manager.gd
+++ push-to-start/scripts/level_list_and_state_manager.gd
@@ -1,14 +1,18 @@
-extends LevelListManager
+extends LevelManager
 
-func set_current_level_id(value : int) -> void:
-	super.set_current_level_id(value)
-	GameState.level_reached(value)
+func set_current_level_path(value : String) -> void:
+	super.set_current_level_path(value)
+	GameStateExample.set_current_level(value)
+	GameStateExample.get_level_state(value)
 
-func get_current_level_id() -> int:
-	current_level_id = GameState.get_current_level() if force_level == -1 else force_level
-	return current_level_id
+func get_current_level_path() -> String:
+	var state_level_path := GameStateExample.get_current_level_path()
+	if not state_level_path.is_empty():
+		current_level_path = state_level_path
+	return super.get_current_level_path()
 
 func _advance_level() -> bool:
 	var _advanced := super._advance_level()
-	GameState.set_current_level(current_level_id)
+	if _advanced:
+		GameStateExample.level_reached(current_level_path)
 	return _advanced
